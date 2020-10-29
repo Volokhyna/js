1b70b035653f4914b422b83fc0da3f7f4da58abb
Volokhyna <volokhyna28alina@gmail.com>
2020-10-29 18:05:34 +0200

    js hw1

---
+++ js1.js
@@ -0,0 +1,110 @@
+let greeting = "hello";
+let university = "own";
+let login = "com";
+let national = "ua";
+let number1 = 1;
+let number2 = 10;
+let number3 = -999;
+let number4 = 123;
+let number5 = 3.14;
+let number6 = 2.7;
+let number7 = 16;
+let tx = true;
+let tx2 = false;
+console.log(greeting); document.write(greeting); alert(greeting);
+console.log(university); document.write(university); alert(university);
+console.log(login); document.write(login); alert(login);
+console.log(national); document.write(national); alert(national);
+console.log(number1); document.write(number1); alert(number1);
+console.log(number2); document.write(number2); alert(number2);
+console.log(number3); document.write(number3); alert(number3);
+console.log(number4); document.write(number4); alert(number4);
+console.log(number5); document.write(number5); alert(number5);
+console.log(number6); document.write(number6); alert(number6);
+console.log(number7); document.write(number7); alert(number7);
+
+let unknown = undefined;
+console.log (unknown); document.write(unknown); alert(unknown);
+
+const nb1 = 43;
+const nb2 = 59;
+const nb3 = 7;
+const ab = "abrakadabra";
+const dog = "dog";
+const cat = "cat";
+console.log(nb1); document.write(nb1); alert(nb1);
+console.log(nb2); document.write(nb2); alert(nb2);
+console.log(nb3); document.write(nb3); alert(nb3);
+console.log(ab); document.write(ab); alert(ab);
+console.log(dog); document.write(dog); alert(dog);
+console.log(cat); document.write(cat); alert(cat);
+
+let surname = prompt("Volokhyna");
+let name = prompt("Alina");
+let otchestvo = prompt("Igorivna");
+console.log(surname); document.write(surname); alert(surname);
+console.log(name); document.write(name); alert(name);
+console.log(otchestvo); document.write(otchestvo); alert(otchestvo);
+
+let person = surname+name+otchestvo;
+console.log(person); document.write (person); alert(person);
+
+let test1 = +prompt("100");
+let test2 = +prompt("200");
+let test3 = +prompt("300");
+console.log(test1);
+console.log(test2);
+console.log(test3);
+
+let x1 = parseInt(3.3);
+let x2 = parseInt(86.7);
+let x3 = parseInt(7.2);
+let x4 = parseInt(92.9);
+console.log(x1);
+console.log(x2);
+console.log(x3);
+console.log(x4);
+let result = x1+x2+x3+x4;
+console.log(result);
+
+let y1 = parseFloat(1.5);
+let y2 = parseFloat(2.3);
+let y3 = parseFloat(4.8);
+console.log(y1);
+console.log(y2);
+console.log(y3);
+let result2 = y1+y2+y3;
+console.log(result2);
+
+let z1 = Math.round(7.9);
+let z2 = Math.round(84.7);
+let z3 = Math.round(5.4);
+console.log(z1);
+console.log(z2);
+console.log(z3);
+let result3 = z1+z2+z3;
+console.log(result3);
+
+let xz = Math.pow(x1, z3);
+console.log(xz);
+
+let a = 100;
+let b = "100";
+let c = true;
+let d = undefined;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 == 6);
+console.log(5 >= 6);
+console.log(10 >= 10);
+console.log(10 == 10);
+console.log(10 != 10);
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(123 != "123");
+console.log(123 == "123");
