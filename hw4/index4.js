b928fcd3053e1bd98987fe861fac28b1ea5db653
Volokhyna <volokhyna28alina@gmail.com>
2020-11-09 15:52:58 +0200

    hw4

--- hw4/index4.js
+++ hw4/index4.js
@@ -1,9 +1,8 @@
 // - створити функцію яка виводить масив
-let array = [];
 function thisFunction() {
-    console.log(array);
+    console.log(arguments);
 }
-thisFunction(array);
+thisFunction([1, 2, 3]);
 
 // - створити функцію яка заповнює масив рандомними числами та виводить його.
 function isfunction(length, min, max) {
@@ -25,7 +24,7 @@ function fun(a, b, c) {
     return min;
 }
 let x = fun(15, 7, 9);
-console.log(x);
+console.log('min', x);
 
 // - створити функцію яка приймає три числа та виводить та повертає найбільше.
 function fun2(a, b, c) {
@@ -36,7 +35,7 @@ function fun2(a, b, c) {
     return max;
 }
 let y = fun2(15, 7, 9);
-console.log(y);
+console.log('max', y);
 
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
 function minandmax() {
@@ -46,45 +45,87 @@ function minandmax() {
         if (item > max) max = item;
         if (item < min) min = item;
     }
-    console.log(max);
+    console.log('max', max);
     return min;
 }
 let min = minandmax(123, 87, 4, 130, 93);
-console.log(min);
+console.log('min', min);
 
 // - створити функцію яка повертає найбільше число з масиву
+let funct = (array) => {
+    let max = array[0];
+    for (const item of array) {
+        if (item > max) max = item
+    }
+    return max;
+}
+let funct2 = funct([34, 9, 529, 78, 124]);
+console.log('max', funct2);
+
 // - створити функцію яка повертає найменьше число з масиву
+let funis = (array) => {
+    let min = array[0];
+    for (const item of array) {
+        if (item < min) min = item
+    }
+    return min;
+}
+let funis2 = funis([34, 9, 529, 78, 124]);
+console.log('min', funis2);
+
 // - створити функцію яка приймає масив чисел та складає значення елементів масиву та повертає його.
+function arraySum(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+let arraySum1 = arraySum([15, 9, 6]);
+console.log(arraySum1);
+
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+function arithmeticMean(arr) {
+    let average = 0;
+    for (let i = 0; i < arr.length; i++) {
+        average += arr[i];
+    }
+    return average / arr.length;
+}
+let arithmetic = arithmeticMean([15, 9, 6]);
+console.log(arithmetic);
+
 // - Створити функцію яка приймає масив будь яких объектів, та повертає значення кількості об'єктів в масиві
-// - Створити функцію яка приймає масив будь яких объектів, та повертає загальн кількість полів в них
+// не понимаю
+// function lengtharray(array) {
+//     for (const item of array) {
+//         if (item === 'object')
+//         return ?
+//     }
+// }
+// let l = lengtharray(['Alina', ['vasya'], true, 143, 'abc', { age: 26 }]);
+// console.log(l);
+
 // - створити функцію  яка скаладає значення елементів з однаковими індексами  та повертає новий результуючий масив.
 //   Приклад
 //   [1,2,3,4]
 //   [2,3,4,5]
 //   результат
 //   [3,5,7,9]
-// - *** приймає масив та число "i", та міняє місцями об`єкт який знаходиться в індексі "i" на "i+1"
-// - *** створити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
-// Двожина масиву від 2 до 100
-// Приклад
-// [1,0,6,0,3] => [1,6,3,0,0]
-// [0,1,2,3,4] => [1,2,3,4,0]
-// [0,0,1,0]   => [1,0,0,0]
+function sumatorarray(array1, array2) {
+    let result = [];
+    for (let i = 0; i < array1.length; i++) {
+        result.push(array1[i] + array2[i]);
+    }
+    return result;
+}
+let sumator = sumatorarray([1, 2, 3, 4], [2, 3, 4, 5]);
+console.log(sumator);
 
-// Створити функцію яка :
-// - Додає в боді блок з текстом "Hello owu"
-// - Додає в боді елемент з текстом . Тип елементу та текст отримати через аргументи
-// - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
-// Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали. Всі властивості авто в обному блоці
-// - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
-// Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали.
-// Для кожної властивості створити всередені блока автомоблія свій блок
-
-// (на основі минулого ДЗ)
-// **- функція приймає 2 масиви з рівною кількістю об'єктів та з'єднює в один об'єкт користувача та місто з відповідними "id" та "user_id",
-// та повертає масив цих з'єднаних об'єктів.
-// Приклад масивів:
-//             let usersWithId = [{id: 1, name: 'vasya', age: 31, status: false}, {id: 2, name: 'petya', age: 30, status: true}, {id: 3, name: 'kolya', age: 29, status: true}, {id: 4, name: 'olya', age: 28, status: false},];
-//             let citiesWithId = [{user_id: 3, country: 'USA', city: 'Portland'}, {user_id: 1, country: 'Ukraine', city: 'Ternopil'}, {user_id: 2, country: 'Poland', city: 'Krakow'}, {user_id: 4, country: 'USA', city: 'Miami'},];
-// Частковий приклад реультату:
+// Створити функцію яка додає в боді елемент з текстом . Тип елементу та текст отримати через аргументи
+function divka(message, typeTag) {
+    let mydiv = document.createElement(typeTag);
+    mydiv.innerText = message('Hello owu');
+    document.body.appendChild(mydiv);
+}
+divka(String, 'div');
